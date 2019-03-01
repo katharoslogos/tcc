2d4e6d469f5d819af67be7b97d6b2be31973363f
katharoslogos <46325797+katharoslogos@users.noreply.github.com>
2019-02-28 22:11:23 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="homeStyle.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+</head>
+<body>
+<div id="wrap">
+		<nav class="nav-bar">
+			<div class="item"><a href="#"><img src="comidaicon.png"></a></div>
+         </nav>
+         <div id="login">
+            <form>
+                <br>
+                <h2>LOGIN</h2>
+                <input type="text" placeholder="usuário: " name=""><br>
+                <input type="password" placeholder="senha: " name=""><br>       
+                <button submit="">ENTRAR</button>
+            </form>
+         </div>
+</div>          
+</body>
+</html>
